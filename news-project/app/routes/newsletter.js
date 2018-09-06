c1cd55fc348feec5f90658a3fc1ef10a4391889f
Day Souza <day.carol18@gmail.com>
2018-09-06 13:00:06 -0300

    Addes .gitignore

--- news-project/app/routes/newsletter.js
+++ news-project/app/routes/newsletter.js
@@ -1,6 +1,16 @@
 module.exports = function (app) {
   app.get('/newsletter', function (req, res) {
-    res.render("news/newsletter");
+
+  var mysql = require('mysql');
+  var connect_str = require('../../config/mysql_config');
+console.log(connect_str());
+  var connection = mysql.createConnection(connect_str());
+
+  connection.query("select * from tab_news",function(error,result){
+    res.send(result);
+  });
+
+    //res.render("news/newsletter");
   });
 
 }
---
+++ news-project/config/mysql_config.js
@@ -0,0 +1,9 @@
+
+module.exports = function () {
+  return mysql = {
+    host: 'localhost',
+    user: 'root',
+    password: 'keymaster',
+    database: 'news_portal'
+  };
+}
