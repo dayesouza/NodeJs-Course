7dad4b4b77b00c5e7e8c6cc383f3695bdce12b86
Day Souza <day.carol18@gmail.com>
2018-12-12 12:57:06 -0200

    Using crypto to store user password

--- mmorpg_GOT_PT_BR/app/models/UsuariosDAO.js
+++ mmorpg_GOT_PT_BR/app/models/UsuariosDAO.js
@@ -1,3 +1,6 @@
+/*Import module */
+var crypto = require("crypto");
+
 function UsuariosDAO(connection) {
   this._database = connection();//Underline é convenção para dizer que faz parte da função --restrição
 }
@@ -6,6 +9,8 @@ UsuariosDAO.prototype.inserirUsuario = function (usuario) {//Metodo da classe
 
   this._database.open(function(err, mongoclient){//abri conexao com o servidor e db
     mongoclient.collection("usuarios", function(err, collection){//Pega a coleção
+
+      usuario.senha = criptografar(usuario.senha);
       collection.insert(usuario);
 
       mongoclient.close();
@@ -14,10 +19,15 @@ UsuariosDAO.prototype.inserirUsuario = function (usuario) {//Metodo da classe
 
 }
 
+function criptografar(dado){
+  return crypto.createHash("md5").update(dado).digest("hex");
+}
+
 UsuariosDAO.prototype.autenticar = function (usuario, req, res) {//Metodo da classe
   this._database.open(function(err, mongoclient){//abri conexao com o servidor e db
     mongoclient.collection("usuarios", function(err, collection){//Pega a coleção
-        // collection.find({usuario:  usuario.usuario, senha:  usuario.senha}).toArray(function(err, result){
+        
+      usuario.senha = criptografar(usuario.senha);
         collection.find(usuario).toArray(function(err, result){
           if(result[0] != undefined){
             req.session.autorizado = true;
